e247f429b30521a725a3b6b50786320652a2e0d4
Andrew Casey <accasey@hotmail.com.au>
2022-08-13 12:52:52 +0100

    Finished ch3

---
+++ rentomtic/repository/memrepo.py
@@ -0,0 +1,9 @@
+from rentomtic.domain.room import Room
+
+
+class MemRepo:
+    def __init__(self, data):
+        self.data = data
+
+    def list(self):
+        return [Room.from_dict(i) for i in self.data]
